00faf98b5b467877ff8e0b0826fe36e018361b02
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2018-03-03 04:49:53 +0100

    Add PoW
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
   "fmt"
+  "strconv"
+  
   mc "github.com/drasko/minichain"
 )
 
@@ -15,6 +17,8 @@ func main() {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+    pow := mc.NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
